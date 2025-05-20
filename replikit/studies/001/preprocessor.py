186a7d1b37e3360576a989f80716614833862cbf
Florian Angermeir <florian@angermeir.me>
2025-05-20 17:33:30 +0200

    Dummy study

---
+++ replikit/studies/001/preprocessor.py
@@ -0,0 +1,12 @@
+from base.preprocessor import StudyPreprocessor
+
+class Preprocessor(StudyPreprocessor):
+    
+    def __init__(self, config):
+        super().__init__(config)
+
+    def _configure(self):
+        pass
+
+    def magic(self):
+        print("Preprocessing everthing...")
