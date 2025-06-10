2a172a4614a6eab9a56c22667899ce105615b91e
Florian Angermeir <florian@angermeir.me>
2025-06-10 09:32:19 +0200

    First five runs for study 009 with gpt 4

--- replikit/studies/009/entrypoint.sh
+++ replikit/studies/009/entrypoint.sh
@@ -3,7 +3,7 @@
 languages=("Java" "C++" "C" "Go")
 
 for tgt in "${languages[@]}"; do
-    echo "Translating from $src to $tgt"
+    echo "Translating from Python to $tgt"
     bash scripts/translate.sh GPT-4 codenet Python "$tgt" 50 0.95 0.7 0 && \
     bash scripts/test_codenet.sh Python "$tgt" GPT-4 fix_reports 1
     cp -r fix_reports /tmp/
